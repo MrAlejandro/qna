ff3ca7e6043823246c52cfe85feb6492b1c7797a
Alexander Shcherbachenko <ascherbachenko@cs-cart.com>
2019-05-25 12:44:56 +0400

    Authorization added to the project

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,8 @@ class CommentsController < ApplicationController
   before_action :set_commentable, only: :create
   after_action :publish_comment, only: :create
 
+  authorize_resource
+
   def create
     @comment = @commentable.comments.new(comment_params)
     @comment.author = current_user
---
+++ app/models/ability.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    @user = user
+
+    if @user
+      @user.admin? ? admin_abilities : user_abilities
+    else
+      guest_abilities
+    end
+  end
+
+  def guest_abilities
+    can :read, :all
+  end
+
+  def admin_abilities
+    can :manage, :all
+  end
+
+  def user_abilities
+    guest_abilities
+    can :create, [Question, Answer, Comment]
+    can :update, [Question, Answer], author: @user
+  end
+end
